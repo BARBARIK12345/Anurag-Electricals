93bb7184beec7ab3e1db90e4aba77f9c1ef27329
BARBARIK12345 <ktotre@gmail.com>
2023-08-03 15:31:29 +0530

    Nav Bar Initialized

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,34 @@
+import { HStack, Box, Image } from "@chakra-ui/react";
+import HeadingItems from "./HeadingItems";
+import logo from "../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <div>
+      <HStack
+        justifyContent={"space-between"}
+        padding={5}
+        paddingBottom={0}
+        pos={"fixed"}
+        top={0}
+        width={"100%"}
+      >
+        <Box>
+          <Image
+            boxSize={"fit-content"}
+            objectFit="cover"
+            src={logo}
+            alt="Logo"
+          />
+        </Box>
+        <Box marginRight={16}>
+          <HStack>
+            <HeadingItems />
+          </HStack>
+        </Box>
+      </HStack>
+    </div>
+  );
+};
+
+export default NavBar;
